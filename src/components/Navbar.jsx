c4225c0a8826d4957c436c40be491aa351a67b99
aryoyudhanto <aryoyudhanto25399@gmail.com>
2023-03-17 17:32:28 +0700

    restyle: restyle login logout with ternary if cookie.token exist

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { useCookies } from "react-cookie";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
+  const [cookie, setCookie, removeCookie] = useCookies();
+  const router = useRouter();
+
+  function onLogout() {
+    Swal.fire({
+      title: "Are you sure want to logout?",
+      // text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Yes",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          text: "Logout successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        removeCookie("token");
+        router.push("/");
+      }
+    });
+  }
   return (
-    <div className="navbar bg-white shadow-md">
+    <div className="navbar bg-white shadow-md sticky top-0 z-50">
       <div className="flex-1 ml-10">
         <img
           src={
@@ -14,7 +44,7 @@ const Navbar = () => {
       </div>
       <div className="flex-none mr-5">
         <ul className="menu menu-horizontal p-0">
-          <li className="text-blue-900 font-semibold">
+          <li className="text-black font-semibold">
             <a>Home</a>
           </li>
         </ul>
@@ -30,10 +60,11 @@ const Navbar = () => {
           className="mt-3 p-2 shadow-xl menu menu-compact dropdown-content bg-white rounded-box w-52"
         >
           <li className="text-black">
-            <p>Settings</p>{" "}
-          </li>
-          <li className="text-black">
-            <p>Logout</p>{" "}
+            {cookie.token ? (
+              <p className="active:bg-gray-500" onClick={() => onLogout()}>Logout</p>
+            ) : (
+              <p className="active:bg-gray-500" onClick={()=>router.push('/')}>Login</p>
+            )}
           </li>
         </ul>
       </div>
