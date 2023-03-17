7b34d966cae8a7fb1b5dbaf0e610dfc0ab91140c
aryoyudhanto <aryoyudhanto25399@gmail.com>
2023-03-17 17:35:48 +0700

    feat: integration user list api with axios and react query, added new feature like and dislike, added feature 4 user list from 6 user list, added feature pagination, added feature can see user list data but can't like or dislike

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import { CookiesProvider } from 'react-cookie'
+import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 
 export default function App({ Component, pageProps }) {
-  return <CookiesProvider><Component {...pageProps} /></CookiesProvider>
+  const queryClient = React.useRef(new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient.current}>
+        <CookiesProvider><Component {...pageProps} /></CookiesProvider>
+    </QueryClientProvider>
+  )
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { useQueryClient, useQuery } from "react-query";
+import axios from "axios";
+
+import Layout from "@/components/Layout";
+import Navbar from "@/components/Navbar";
+import Card from "@/components/Card";
+
+const Home = () => {
+  const [page, setPage] = useState(1);
+  const queryClient = useQueryClient();
+
+  const getUsers = async (page) => {
+    const response = await axios.get(
+      `https://reqres.in/api/users?page=${page}`
+    );
+    return response.data.data.map((user) => ({
+      ...user,
+      liked: false,
+      disliked: false,
+    }));
+  };
+
+  const {
+    data: users,
+    data,
+    isLoading,
+  } = useQuery(["users", page], () => getUsers(page), {
+    refetchOnWindowFocus: false,
+  });
+
+  const handleLike = (user) => {
+    if (user.liked != true) {
+      const updatedUser = { ...user, liked: true, disliked: false };
+      queryClient.setQueryData(["users", page], (oldData) =>
+        oldData.map((u) => (u.id === user.id ? updatedUser : u))
+      );
+    } else {
+      const updatedUser = { ...user, liked: false, disliked: false };
+      queryClient.setQueryData(["users", page], (oldData) =>
+        oldData.map((u) => (u.id === user.id ? updatedUser : u))
+      );
+    }
+  };
+
+  const handleDislike = (user) => {
+    if (user.disliked != true) {
+      const updatedUser = { ...user, liked: false, disliked: true };
+      queryClient.setQueryData(["users", page], (oldData) =>
+        oldData.map((u) => (u.id === user.id ? updatedUser : u))
+      );
+    } else {
+      const updatedUser = { ...user, liked: false, disliked: false };
+      queryClient.setQueryData(["users", page], (oldData) =>
+        oldData.map((u) => (u.id === user.id ? updatedUser : u))
+      );
+    }
+  };
+
+  const handlePreviousPage = () => setPage((prev) => Math.max(prev - 1, 1));
+  const handleNextPage = () => setPage((prev) => prev + 1);
+  if (page > 2) {
+    setPage(2);
+  }
+
+  if (isLoading)
+    return (
+      <div className="text-5xl text-center animate-pulse text-gray-400 my-24">
+        Loading...
+      </div>
+    );
+  if (!data)
+    return (
+      <div className="text-5xl text-center animate-pulse text-gray-400 my-24">
+        No data!
+      </div>
+    );
+
+  return (
+    <Layout>
+      <Navbar />
+      <div className="mx-14 mt-14">
+        <p className="text-xl font-semibold text-black capitalize">user list</p>
+      </div>
+      <div className="m-14 grid grid-rows-4 grid-flow-col gap-2">
+        {data.slice(0, 4).map((user) => (
+          <Card
+            key={user.id}
+            image={user.avatar}
+            name={user.first_name + " " + user.last_name}
+            like={() => handleLike(user)}
+            colorLike={user.liked === true ? "text-red-500" : "text-black"}
+            dislike={() => handleDislike(user)}
+            colorDislike={
+              user.disliked === true ? "text-red-500" : "text-black"
+            }
+          />
+        ))}
+      </div>
+      <div className="btn-group w-full justify-center mb-10">
+        <button
+          className="btn bg-[#f5f5f5] text-black hover:text-white border-black border-r-0"
+          onClick={() => handlePreviousPage()}
+        >
+          «
+        </button>
+        <button className="btn bg-[#f5f5f5] text-black hover:text-white border-black border-r-0 border-l-0">
+          {page}
+        </button>
+        <button
+          className="btn bg-[#f5f5f5] text-black hover:text-white border-black border-l-0"
+          onClick={() => handleNextPage()}
+        >
+          »
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
