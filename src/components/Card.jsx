422baca4c293fd192f0f2497985b1e3ffac00b3e
aryoyudhanto <aryoyudhanto25399@gmail.com>
2023-03-18 08:54:36 +0700

    style: restye card reuseable component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { useCookies } from "react-cookie";
 import Image from "next/image";
 import React from "react";
 
-const Card = ({ image, name, colorDislike, colorLike, dislike, like }) => {
+const Card = ({ onclikDetail, image, name, colorDislike, colorLike, dislike, like }) => {
   const [cookie, setCookie] = useCookies();
   return (
     <div className="card card-side h-60 shadow-xl mb-5">
@@ -11,14 +11,15 @@ const Card = ({ image, name, colorDislike, colorLike, dislike, like }) => {
         <Image
           src={image}
           alt="Avatar"
-          className="p-5"
-          width={200}
-          height={200}
+          className="p-5 hover:cursor-pointer"
+          width={300}
+          height={300}
+          onClick={onclikDetail}
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title my-5">{name}</h2>
-        <p></p>
+        <h2 className="card-title my-5 hover:cursor-pointer" onClick={onclikDetail}>{name}</h2>
+        <p onClick={onclikDetail} className="hover:cursor-pointer"></p>
         {cookie.token ? (
           <div className="card-actions justify-end items-end">
             <AiFillDislike
